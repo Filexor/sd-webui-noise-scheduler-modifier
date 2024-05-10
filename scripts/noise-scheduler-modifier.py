80b6826d3c661b488b3b313b7d67e633573a0248
File_xor <filexor@gmail.com>
2024-05-10 23:27:20 +0900

    bugfix

--- scripts/noise-scheduler-modifier.py
+++ scripts/noise-scheduler-modifier.py
@@ -97,7 +97,7 @@ class Noise_scheduler_modifier(scripts.Script):
         sigmas = None
         for i in range(4):
             if _enable[i]:
-                if not _use_raw_sigma[i] and not isinstance(_start_value[i], int) or not isinstance(_end_value[i], int):
+                if not _use_raw_sigma[i]:
                     _start_value[i] = int(_start_value[i])
                     _end_value[i] = int(_end_value[i])
                     if _start_value[i] < 0 or _start_value[i] > 999 or _end_value[i] < 0 or _end_value[i] > 999:
